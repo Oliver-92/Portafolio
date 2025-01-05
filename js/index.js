6138be90cdb578a61413f5f08dd53ca8940bf027
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2025-01-05 19:35:38 -0300

    Responsive details corrections

--- js/index.js
+++ js/index.js
@@ -14,6 +14,21 @@ typewriter
     .start();
 
 
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    const scrollPosition = window.scrollY;
+
+    // Cambia la clase según la posición de desplazamiento
+    if (scrollPosition > 50) { // Ajusta el valor según cuándo quieres el cambio
+        navbar.classList.add('navbar-opaque');
+        navbar.classList.remove('navbar-transparent');
+    } else {
+        navbar.classList.add('navbar-transparent');
+        navbar.classList.remove('navbar-opaque');
+    }
+});
+
+
 // Función para alternar entre secciones
 document.getElementById('developer-btn').addEventListener('click', function () {
     document.getElementById('developer').style.display = 'flex';  // Muestra la sección de Desarrollador Web
