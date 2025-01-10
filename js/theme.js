2f0a714806e14525c234b13eb609743e2c56b844
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2025-01-10 19:06:02 -0300

    light mode

--- js/theme.js
+++ js/theme.js
@@ -4,21 +4,21 @@ const body = document.body;
 // Verificar si el modo oscuro o claro está guardado en localStorage
 if (localStorage.getItem('theme') === 'light') {
     body.setAttribute('data-theme', 'light');
-    toggleButton.textContent = 'Modo Oscuro';  // Cambiar texto del botón
+    toggleButton.textContent = 'DARK MODE';  // Cambiar texto del botón
 } else {
     body.setAttribute('data-theme', 'dark');  // Modo oscuro por defecto
-    toggleButton.textContent = 'Modo Claro';  // Cambiar texto del botón
+    toggleButton.textContent = 'LIGHT MODE';  // Cambiar texto del botón
 }
 
 // Agregar un evento al botón para alternar entre los modos
 toggleButton.addEventListener('click', () => {
     if (body.getAttribute('data-theme') === 'dark') {
         body.setAttribute('data-theme', 'light');
-        toggleButton.textContent = 'Modo Oscuro';  // Cambiar texto del botón
+        toggleButton.textContent = 'DARK MODE';  // Cambiar texto del botón
         localStorage.setItem('theme', 'light');  // Guardar la preferencia
     } else {
         body.setAttribute('data-theme', 'dark');
-        toggleButton.textContent = 'Modo Claro';  // Cambiar texto del botón
+        toggleButton.textContent = 'LIGHT MODE';  // Cambiar texto del botón
         localStorage.setItem('theme', 'dark');  // Guardar la preferencia
     }
 });
