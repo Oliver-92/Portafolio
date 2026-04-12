3dfa290d982d36424807439c1ba804e73984c488
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-04-12 11:43:31 -0300

    refactoring badges

--- src/components/molecules/BadgeContainer.tsx
+++ src/components/molecules/BadgeContainer.tsx
@@ -1,23 +1,11 @@
+import badges from "../../utils/badges"
+
 export default function BadgeContainer() {
     return (
         <span className="flex justify-center gap-1 flex-wrap max-w-[1200px] p-1">
-            <img src="https://img.shields.io/badge/HTML-%23E34F26.svg?logo=html5&logoColor=white" alt="logo-HTML" />
-            <img src="https://img.shields.io/badge/CSS-639?logo=css&logoColor=fff" alt="logo-CSS" />
-            <img src="https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=000" alt="logo-JavaScript" />
-            <img src="https://img.shields.io/badge/TypeScript-3178C6?style=flat&logo=typescript&logoColor=white" alt="logo-TypeScript" />
-            <img src="https://img.shields.io/badge/Python-3776AB?logo=python&logoColor=fff" alt="logo-Python" />
-            <img src="https://img.shields.io/badge/MySQL-4479A1?logo=mysql&logoColor=fff" alt="logo-MySQL" />
-            <img src="https://img.shields.io/badge/SQLite-%2307405e.svg?logo=sqlite&logoColor=white" alt="logo-SQLite" />
-            <img src="https://img.shields.io/badge/React-%2320232a.svg?logo=react&logoColor=%2361DAFB" alt="logo-React" />
-            <img src="https://img.shields.io/badge/React_Router-CA4245?logo=react-router&logoColor=white" alt="logo-React-Router" />
-            <img src="https://img.shields.io/badge/React%20Hook%20Form-EC5990?logo=reacthookform&logoColor=fff" alt="logo-React-Hook-Form" />
-            <img src="https://img.shields.io/badge/Next.js-000000?style=flat&logo=nextdotjs&logoColor=white" alt="logo-Next.js" />
-            <img src="https://img.shields.io/badge/Tailwind%20CSS-%2338B2AC.svg?logo=tailwind-css&logoColor=white" alt="logo-Tailwind-CSS" />
-            <img src="https://img.shields.io/badge/Bootstrap-7952B3?logo=bootstrap&logoColor=fff" alt="logo-Bootstrap" />
-            <img src="https://img.shields.io/badge/Node.js-6DA55F?logo=node.js&logoColor=white" alt="logo-Node.js" />
-            <img src="https://img.shields.io/badge/Flask-000?logo=flask&logoColor=fff" alt="logo-Flask" />
-            <img src="https://img.shields.io/badge/Git-F05032?logo=git&logoColor=fff" alt="logo-Git" />
-            <img src="https://img.shields.io/badge/Vite-646CFF?logo=vite&logoColor=fff" alt="logo-Vite" />
+            {Object.entries(badges).map(([name, url]) => (
+                <img key={name} src={url} alt={`logo-${name}`} />
+            ))}
         </span>
     )
 }
