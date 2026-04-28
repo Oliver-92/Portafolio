5da0ce5be6ee00cfb9149403591a0ea75c6a645b
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-04-28 20:24:43 -0300

    perfomance fonts update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import "@fontsource/coda/400.css";
-import "@fontsource/fira-code/400.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
